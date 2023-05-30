b936f4723730c405d95be04033aa7449be17ad44
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-30 18:46:32 +0300

    feat : add new book

--- src/main/java/org/gassion/LibrarySpringApp/controller/BookController.java
+++ src/main/java/org/gassion/LibrarySpringApp/controller/BookController.java
@@ -1,10 +1,13 @@
 package org.gassion.LibrarySpringApp.controller;
 
 import org.gassion.LibrarySpringApp.dao.BookDAO;
+import org.gassion.LibrarySpringApp.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +24,17 @@ public class BookController {
     public String getAllBooks(Model model) {
         model.addAttribute("books", bookDAO.getAll());
         return "book/all";
+    }
+
+    @GetMapping("/new")
+    public String addNewBookView(Model model) {
+        model.addAttribute("newBook", new Book());
+        return "book/new";
+    }
+
+    @PostMapping
+    public String addNewBook(@ModelAttribute("newBook") Book newBook) {
+        bookDAO.add(newBook);
+        return "redirect:/book";
     }
 }
